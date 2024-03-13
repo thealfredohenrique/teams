2a453e012a614e465f795fd8c0d63582217badce
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-03-12 22:44:16 -0300

    feat: add theme with style guide
    * This allows for consistent styling across the app.

---
+++ src/theme/index.ts
@@ -0,0 +1,26 @@
+export default <const>{
+  colors: {
+    gray100: "#e1e1e6",
+    gray200: "#c4c4cc",
+    gray300: "#7c7c8a",
+    gray400: "#323238",
+    gray500: "#29292e",
+    gray600: "#202024",
+    gray700: "#121214",
+    green500: "#00b37e",
+    green700: "#00875f",
+    red500: "#f75a68",
+    red700: "#aa2834",
+    white: "#ffffff",
+  },
+  fontFamily: {
+    regular: "Roboto_400Regular",
+    bold: "Roboto_700Bold",
+  },
+  fontSize: {
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24,
+  },
+};
