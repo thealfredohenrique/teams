e1f69e73bad099440e675faddf6086b292c421d5
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-03-13 21:21:46 -0300

    feat: Add Google's Roboto font using expo-font
    * Integrate the Roboto font from Google using the expo-font library for enhanced typography in the project.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { Groups } from "@screens/Groups";
 import theme from "@themes/default";
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {isFontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
