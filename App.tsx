9432304ba55307d7ccf4a47f4497674850aa93a6
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-03-13 21:30:55 -0300

    feat: Add Loading component
    * Implemented a Loading component to display loading indicators and improve user experience during data retrieval or processing.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { Loading } from "@components/Loading";
 import { Groups } from "@screens/Groups";
 import theme from "@themes/default";
 
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {isFontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {isFontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
