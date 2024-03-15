3896aff4529df8245c2b2d225bf52e699f01c5f1
Alfredo Henrique <thealfredohenrique@gmail.com>
2024-03-14 21:46:35 -0300

    feat: Add Players screen
    * Implemented the Players screen to display information and manage players within the application.

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
-import { Groups } from "@screens/Groups";
+import { Players } from "@screens/Players";
 import theme from "@themes/default";
 
 export default function App() {
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {isFontsLoaded ? <Groups /> : <Loading />}
+      {isFontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
